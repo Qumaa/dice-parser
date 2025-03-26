9f7ddcd221ba43d9e98aa36b7f8ba47ec77d5c6f
Qma <mk348437@gmail.com>
2025-03-26 20:30:20 +0100

    Move primitive data types to respective folder

---
+++ DiceRoll/Expressions/Primitive types/ProbabilityOf.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DiceRoll.Expressions
+{
+    [StructLayout(LayoutKind.Auto)]
+    public readonly struct ProbabilityOf<T>
+    {
+        public readonly T Value;
+        public readonly Probability Probability;
+        
+        public ProbabilityOf(T value, Probability probability)
+        {
+            Value = value;
+            Probability = probability;
+        }
+    }
+}
