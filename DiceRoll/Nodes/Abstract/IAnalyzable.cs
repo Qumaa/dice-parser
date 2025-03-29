0693e90e868f04394809abcd7ec2b97b016250d6
Qma <mk348437@gmail.com>
2025-03-30 00:31:12 +0100

    Update abstract docs

--- DiceRoll/Nodes/Abstract/IAnalyzable.cs
+++ DiceRoll/Nodes/Abstract/IAnalyzable.cs
@@ -4,11 +4,12 @@
     /// <para>
     /// Base interface for any numerical node that takes part in expressions.
     /// A shorthand for implementing <see cref="IRollable"/> and <see cref="IDistributable{T,TType}">IDistributable</see>
-    /// of the <see cref="Roll"/> type.
+    /// of type <see cref="Roll"/>.
     /// </para>
     /// <para>
     /// Implementations are expected to <see cref="INode{T}.Evaluate">evaluate</see> themselves to <see cref="Outcome"/>
-    /// and provide a <see cref="RollProbabilityDistribution">distribution</see> of every possible evaluation value.
+    /// and provide a <see cref="RollProbabilityDistribution">probability distribution</see> of every possible
+    /// evaluation value.
     /// </para>
     /// </summary>
     public interface IAnalyzable : IRollable, IDistributable<RollProbabilityDistribution, Roll> { }
--- DiceRoll/Nodes/Abstract/IOperation.cs
+++ DiceRoll/Nodes/Abstract/IOperation.cs
@@ -1,7 +1,25 @@
 ﻿namespace DiceRoll.Nodes
 {
+    /// <summary>
+    /// <para>
+    /// Base interface for any node of binary operation between two <see cref="IAnalyzable">numerical nodes</see>
+    /// that takes part in expressions.
+    /// Implements <see cref="INode{T}">INode</see> of type <see cref="Binary"/>
+    /// and <see cref="IDistributable{T,TType}">IDistributable</see> of type <see cref="Logical"/>.
+    /// </para>
+    /// <para>
+    /// Implementations are expected to <see cref="INode{T}.Evaluate">evaluate</see> themselves to <see cref="Binary"/>,
+    /// provide a <see cref="RollProbabilityDistribution">probability distribution</see> of true and false,
+    /// and provide an <see cref="OperationVerboseEvaluation"/> instance.
+    /// </para>
+    /// </summary>
     public interface IOperation : INode<Binary>, IDistributable<LogicalProbabilityDistribution, Logical>
     {
+        /// <summary>
+        /// Provides a verbose evaluation result that complements the operation result with numeric values of both
+        /// nodes.
+        /// </summary>
+        /// <returns>An <see cref="OperationVerboseEvaluation"/> instance.</returns>
         OperationVerboseEvaluation EvaluateVerbose();
     }
 }
