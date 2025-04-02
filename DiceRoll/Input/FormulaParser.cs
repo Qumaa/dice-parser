40229dc4378a58e57865ba0af80a81537b59c1cf
Qma <mk348437@gmail.com>
2025-04-02 16:12:23 +0200

    WIP

---
+++ DiceRoll/Input/FormulaParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DiceRoll.Input
+{
+    public class FormulaParser
+    {
+        public bool TryParse<T>(string[] args, int start, out int end, out IVisitableNode visitableNode)
+        {
+            
+        }
+    }
+
+    public interface IVisitableNode
+    {
+        void Visit(INodeVisitor nodeVisitor);
+    }
+
+    public static class NodeExtensions
+    {
+        public static void Visit<T>(this INode<T> node, INodeVisitor nodeVisitor)
+        {
+            switch (node)
+            {
+                case IAnalyzable analyzable:
+                    nodeVisitor.ForNumeric(analyzable);
+                    break;
+
+                case IOperation operation:
+                    nodeVisitor.ForOperation(operation);
+                    break;
+
+                case IConditional conditional:
+                    nodeVisitor.ForConditional(conditional);
+                    break;
+            }
+        }
+    }
+
+    public interface INodeVisitor
+    {
+        void ForNumeric(IAnalyzable analyzable);
+        void ForOperation(IOperation operation);
+        void ForConditional(IConditional conditional);
+    }
+}
