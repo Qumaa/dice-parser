c5ba9d33fe66ef25dfea91ea676a3d6811f3d42e
Qma <mk348437@gmail.com>
2025-04-09 12:18:20 +0200

    Highlight the edge case that causes the evaluation to crash
    Underlying CDF table structure is an array, designed for sequential roll outcomes (1, 2, 3...) and doesn't support non-uniform sequences (5, 10, 20...)

--- DiceRoll/Program.cs
+++ DiceRoll/Program.cs
@@ -6,32 +6,19 @@ namespace DiceRoll
     {
         public static void Main(string[] args)
         {
-            var left = Node.Value.Dice(6);
-            left = Node.Value.Summation(left, left);
-            var right = Node.Value.Dice(6);
+            INumeric left = Node.Value.Dice(6);
+            left = Node.Transformation.Multiply(left, Node.Value.Constant(5));
+            INumeric right = Node.Value.Dice(6);
 
-            IOperation operation = Node.Operator.Equal(left, right);
+            IOperation operation = Node.Operator.GreaterThan(left, right);
 
-            IAssertion explicitAssertion = operation.AsAssertion();
-
-            IAssertion implicitAssertion = (IAssertion) operation;
-
-            TestVisitor visitor = new();
-            
-            explicitAssertion.Visit(visitor); // Assertion
-            implicitAssertion.Visit(visitor); // Operation
-        }
-        
-        private class TestVisitor : INodeVisitor
-        {
-            public void ForNumeric(INumeric numeric) =>
-                throw new InvalidOperationException();
-
-            public void ForAssertion(IAssertion assertion) =>
-                Console.WriteLine("Assertion");
-
-            public void ForOperation(IOperation operation) =>
-                Console.WriteLine("Operation");
+            foreach (OptionalRoll roll in operation.GetProbabilityDistribution())
+            {
+                Console.Write("Probability of ");
+                Console.Write(roll.Outcome.ToString());
+                Console.Write(" is ");
+                Console.WriteLine(roll.Probability.ToString());
+            }
         }
     }
 }
