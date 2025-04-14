63095b78ca9bfee516ac3cee7c2ad1c3b391dd89
Qma <mk348437@gmail.com>
2025-04-14 03:40:21 +0200

    Rewrite error messages to unify their style

--- DiceRoll/Input/Exceptions/ParsingErrorMessages.cs
+++ DiceRoll/Input/Exceptions/ParsingErrorMessages.cs
@@ -7,36 +7,34 @@ namespace DiceRoll.Input
     {
         public const string UNUSED_OPERAND = "This operand doesn't take part in the expression.";
         public const string TRAILING_DELAYED_OPERATOR = "This operator didn't receive enough right-side operands.";
-        public const string UNBALANCED_PARENTHESIS =
-            "Couldn't close parenthesis: unbalanced parenthesis within the expression.";
+        public const string UNBALANCED_PARENTHESIS = "This parenthesis has no matching opening pair.";
         public const string PUSHING_PREMATURELY =
-            "An attempt to return the result of working on fewer operands than the declared arity was intercepted.";
+            "This operator returned a result before consuming all required operands.";
 
         public static string ExceedingArity(int arity)
         {
-            const string format =
-                "An attempt to work on more operands than the arity of the operator was intercepted. The operator's arity ({0}) is faulty.";
+            const string format = "This operator attempted to consume more operands than its declared arity ({0}).";
 
             return string.Format(format, arity.ToString());
         }
 
         public static string OperandsExpected(int operandsExpected, int operandsReceived)
         {
-            const string format = "Couldn't invoke operator: expected {0} operands, received {1}.";
+            const string format = "This operator expected {0} operand(-s), but received {1}.";
             
             return string.Format(format, operandsExpected.ToString(), operandsReceived.ToString());
         }
         
         public static string UnknownToken(in Substring tokenMatch)
         {
-            const string format = "Couldn't parse the following unknown token: \"{0}\".";
+            const string format = "This token is not recognized (\"{0}\").";
             
             return string.Format(format, tokenMatch.ToString());
         }
         
         public static string OperandTypeMismatch(Type expectedOperandType, Type receivedOperandType)
         {
-            const string format = "Couldn't invoke operator: the operand evaluates to {0} (expected {1}).";
+            const string format = "This operator's operand evaluates to {0}, but {1} was expected.";
             
             return string.Format(format, _FormatTypeName(receivedOperandType), _FormatTypeName(expectedOperandType));
             
