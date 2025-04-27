ce81dcf6a9f040f861da11f568d4abff6fd90520
Qma <mk348437@gmail.com>
2025-04-27 20:37:27 +0200

    Make the program class static

--- DiceRoll/Program.cs
+++ DiceRoll/Program.cs
@@ -2,7 +2,7 @@
 
 namespace DiceRoll
 {
-    public class Program
+    public static class Program
     {
         public static int Main(string[] args) =>
             new DiceCommand(new DiceCommandStrings()).Invoke(args);
