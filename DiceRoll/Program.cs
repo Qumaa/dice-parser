683a6772ae9f1d51d0bcc8bbcf7652c4c26713f7
Qma <mk348437@gmail.com>
2026-07-26 00:23:47 +0400

    Restore args

--- DiceRoll/Program.cs
+++ DiceRoll/Program.cs
@@ -5,6 +5,6 @@ namespace DiceRoll
     public static class Program
     {
         public static int Main(string[] args) =>
-            new DiceCommand(new DiceCommandStrings()).Invoke("roll d20 > 10 ? 20 : 5 -t");
+            new DiceCommand(new DiceCommandStrings()).Invoke(args);
     }
 }
